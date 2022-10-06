1bcefafb6f8bf221e3c2d840f2e60990f842109b
Victor Malyshev <vmalyshev0@gmail.com>
2022-10-06 17:23:09 +0200

    import DB module in tags

--- api-tags/src/app.module.ts
+++ api-tags/src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TagsModule } from './tags/tags.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot(), TagsModule],
 })
 export class AppModule {}
--- api-tags/src/tags/tags.module.ts
+++ api-tags/src/tags/tags.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { TagsController } from './tags.controller';
 import { TagsService } from './tags.service';
 import { ConfigModule } from '@nestjs/config';
+import { tagsProviders } from './tags.provider';
+import { DbModule } from '../db/db.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), DbModule],
   controllers: [TagsController],
-  providers: [TagsService],
+  providers: [...tagsProviders, TagsService],
 })
 export class TagsModule {}
