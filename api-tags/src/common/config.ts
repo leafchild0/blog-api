1503e8f18565703daa47ca823ff651894f991029
Victor Malyshev <vmalyshev0@gmail.com>
2022-10-06 15:29:28 +0200

    config wrapper

---
+++ api-tags/src/common/config.ts
@@ -0,0 +1,12 @@
+/**
+ * Config wrapper, to maybe change and improve it
+ *
+ * @author vmalyshev
+ */
+
+export const BASE_PATH: string = process.env.BASE_PATH;
+export const DOCS_PATH: string = BASE_PATH + '/docs';
+export const SHUTDOWN_TIMEOUT = process.env.SHUTDOWN_TIMEOUT || 10000;
+export const PORT = process.env.PORT || 3000;
+export const API_VERSION = process.env.version || '1.0';
+export const CORS_HEADERS = process.env.CORS_HEADERS || '';
