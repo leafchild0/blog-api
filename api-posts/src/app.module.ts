bd377c8658fee14197859d20e6f7c8cd573f37bc
Victor Malyshev <vmalyshev0@gmail.com>
2022-10-11 18:55:28 +0200

    posts module

--- api-posts/src/app.module.ts
+++ api-posts/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { PostsModule } from './posts/postsModule';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), PostsModule],
