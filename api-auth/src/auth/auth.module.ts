8c343580abfdb700013a6c157e83ba84f690231b
Victor Malyshev <vmalyshev0@gmail.com>
2022-10-13 15:28:45 +0200

    JWT module and strategy usage

--- api-auth/src/auth/auth.module.ts
+++ api-auth/src/auth/auth.module.ts
@@ -3,9 +3,19 @@ import { AuthService } from './auth.service';
 import { UsersService } from '../users/users.service';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { SECRET_KEY, TOKEN_EXPIRATION } from '../common/config';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [UsersService, PassportModule],
-  providers: [AuthService, LocalStrategy],
+  imports: [
+    UsersService,
+    PassportModule,
+    JwtModule.register({
+      secret: SECRET_KEY,
+      signOptions: { expiresIn: TOKEN_EXPIRATION },
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
