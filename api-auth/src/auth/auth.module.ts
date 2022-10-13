7ac61e7f5bb530242528d5fc3a8e6dcc70dcc527
Victor Malyshev <vmalyshev0@gmail.com>
2022-10-13 15:34:28 +0200

    Login endpoint and bug fixed with module export

--- api-auth/src/auth/auth.module.ts
+++ api-auth/src/auth/auth.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { UsersService } from '../users/users.service';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
 import { JwtModule } from '@nestjs/jwt';
 import { SECRET_KEY, TOKEN_EXPIRATION } from '../common/config';
 import { JwtStrategy } from './jwt.strategy';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
-    UsersService,
+    UsersModule,
     PassportModule,
     JwtModule.register({
       secret: SECRET_KEY,
