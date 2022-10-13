6d243b11e3d5f44240eb2e4cc6e1968238ab54e4
Victor Malyshev <vmalyshev0@gmail.com>
2022-10-13 17:25:56 +0200

    sync and crypt

--- api-auth/src/auth/auth.service.ts
+++ api-auth/src/auth/auth.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
+import { UserDto } from '../users/users.dto';
+import { SignUpInput, TokenPayload } from './auth.dto';
+import { hashSync, compareSync } from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -9,19 +12,33 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async validateUser(username: string, pass: string): Promise<any> {
+  async validateUser(username: string, pass: string): Promise<UserDto> {
     const user = await this.usersService.getUserByUsername(username);
-    if (user && user.password === pass) {
-      const { password, ...result } = user;
-      return result;
+    const isPassSame = compareSync(pass, user.password);
+    if (isPassSame) {
+      return {
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt,
+        name: user.name,
+        email: user.email,
+        roles: user.roles,
+        id: user.id,
+      };
     }
     return null;
   }
 
-  async login(user: any) {
+  async login(user: any): Promise<TokenPayload> {
     const payload = { username: user.username, sub: user.userId };
     return {
       access_token: this.jwtService.sign(payload),
     };
+  }
+
+  signUp(input: SignUpInput): Promise<UserDto> {
+    return this.usersService.createUser({
+      ...input,
+      password: hashSync(input.password, 10),
+    });
   }
 }
